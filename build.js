095519c35475d2d25a4e2b6e4f4765157f72cf25
Zongmin Lei <leizongmin@gmail.com>
2014-04-17 10:56:02 +0800

    增加作者简介页面

--- build.js
+++ build.js
@@ -19,27 +19,36 @@ try {
 // 载入数据
 var data = {
   summary: fs.readFileSync('./data/summary.txt', {encoding: 'utf8'}),
-  index: fs.readFileSync('./data/index.txt', {encoding: 'utf8'})
+  index:   fs.readFileSync('./data/index.txt', {encoding: 'utf8'}),
+  authors: fs.readFileSync('./data/author.txt', {encoding: 'utf8'})
 };
 data.index = data.index.split(/\r?\n\r?\n\r?\n/);
 data.index = data.index.map(function (str) {
   var lines = str.trim().split(/\r?\n/);
   return {
     title:   lines[0],
-    author:  parseAuthor(lines[1]),
+    author:  (function (text) {
+                var b = text.split(/\s+/g);
+                return {
+                  name:     b[0],
+                  username: b[1]
+                };
+              })(lines[1]),
     content: lines.slice(2).join('\n')
   };
 });
 data.index.forEach(function (item, i) {
   item.filename = 'pages/' + (i + 1) + '.html';
 });
-function parseAuthor (text) {
-  var b = text.split(/\s+/g);
+data.authors = data.authors.split(/\r?\n\r?\n\r?\n/);
+data.authors = data.authors.map(function (str) {
+  var lines = str.trim().split(/\r?\n/);
   return {
-    name:     b[0],
-    username: b[1]
+    name:     lines[0],
+    username: lines[1],
+    about:    lines.slice(2).join('\n')
   };
-}
+});
 
 // 载入模板
 var render = ejs.compile(fs.readFileSync('./data/template.html', {encoding: 'utf8'}));
@@ -56,10 +65,17 @@ renderPage('index.html', {
 });
 data.index.forEach(function (item) {
   renderPage(item.filename, {
-    isHome: false,
-    data:   item,
-    index:  data.index
+    isSection: true,
+    data:      item,
+    index:     data.index
   });
 });
+renderPage('author.html', {
+  isAuthor: true,
+  data:     data,
+  index:  data.index,
+  authors:  data.authors
+});
+
 
 console.log('OK');
